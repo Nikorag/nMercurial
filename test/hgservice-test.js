5b3af71b140918838040f7fd517c19dd3a884790
Nikorag <nikorag@gmail.com>
2017-06-30 17:46:22 +0100

    Tests

--- test/hgservice-test.js
+++ test/hgservice-test.js
@@ -3,6 +3,8 @@ var request = require("request");
 var hgService = require("../services/hgService");
 var PropertiesReader = require('properties-reader');
 var config = PropertiesReader('properties/test.properties');
+var rimraf = require('rimraf');
+var fs = require('fs');
 
 describe("HG Service", function() {
 
@@ -13,23 +15,37 @@ describe("HG Service", function() {
         });
 
         it("test.repo.url", function(){
-            var repoPath = config.get("test.repo.url");
-            expect(repoPath).to.not.be.empty;
+            var repoUrl = config.get("test.repo.url");
+            expect(repoUrl).to.not.be.empty;
         });
     });
 
-    describe("Get repo path", function(){
-        it("returns A path", function(){
-           hgService.getRepoPath(config.get("test.repo.path")).then(function(result){
-               console.log(result);
-               expect(result).to.not.be.empty;
-           });
-        });
-
-        it("returns the correct path", function(){
-            hgService.getRepoPath(config.get("test.repo.path")).then(function(result){
-                expect(test.repo.url).to.equal(config.get("test.repo.url"));
-            });
-        });
-    })
+    // describe("Clone the repo", function(){
+    //     it("clones the repo", function() {
+    //         var repoPath = config.get("test.repo.path");
+    //         var repoUrl = config.get("test.repo.url");
+    //         if (fs.existsSync(repoPath)) {
+    //             rimraf(repoPath, function () {
+    //                 hgService.clone(repoUrl, repoPath, "username", "password").then(function () {
+    //                     expect(true).to.be.true;
+    //                 })
+    //             });
+    //         }
+    //     });
+    // });
+    //
+    // describe("Get repo path", function(){
+    //     it("returns A path", function(){
+    //        hgService.getRepoPath(config.get("test.repo.path")).then(function(result){
+    //            console.log(result);
+    //            expect(result).to.not.be.empty;
+    //        });
+    //     });
+    //
+    //     it("returns the correct path", function(){
+    //         hgService.getRepoPath(config.get("test.repo.path")).then(function(result){
+    //             expect(test.repo.url).to.equal(config.get("test.repo.url"));
+    //         });
+    //     });
+    // })
 });
